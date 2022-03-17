c624d2320ffa675d521b6cdb551c1abcd2a589f5
BUPT-ANT-1007 <bupt_ant_1007@163.com>
2022-03-17 20:53:30 +0800

    first commit

---
+++ network/resnet101+unet_3.py
@@ -0,0 +1,423 @@
+import os
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '1,0,2,3'
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib
+
+DATA_DIR = ''
+"更换为数据集对应地址"
+# load repo with data if it is not exists
+'''if not os.path.exists(DATA_DIR):
+    print('Loading data...')
+    os.system('git clone https://github.com/alexgkendall/SegNet-Tutorial ./data')
+    print('Done!')'''
+x_train_dir = os.path.join(DATA_DIR, 'train')
+y_train_dir = os.path.join(DATA_DIR, 'trainannot')
+
+x_valid_dir = os.path.join(DATA_DIR, 'val')
+y_valid_dir = os.path.join(DATA_DIR, 'valannot')
+
+x_test_dir = os.path.join(DATA_DIR, 'test')
+y_test_dir = os.path.join(DATA_DIR, 'testannot')
+
+
+# helper function for data visualization
+def visualize(**images):
+    """PLot images in one row."""
+    n = len(images)
+    plt.figure(figsize=(16, 5))
+    for i, (name, image) in enumerate(images.items()):
+        plt.subplot(1, n, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(' '.join(name.split('_')).title())
+        plt.imshow(image)
+    plt.show()
+
+
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset as BaseDataset
+
+
+class Dataset(BaseDataset):
+    """CamVid Dataset. Read images, apply augmentation and preprocessing transformations.
+
+    Args:
+        images_dir (str): path to images folder
+        masks_dir (str): path to segmentation masks folder
+        class_values (list): values of classes to extract from segmentation mask
+        augmentation (albumentations.Compose): data transfromation pipeline
+            (e.g. flip, scale, etc.)
+        preprocessing (albumentations.Compose): data preprocessing
+            (e.g. noralization, shape manipulation, etc.)
+
+    """
+
+
+    CLASSES = ['rock']
+
+    def __init__(
+            self,
+            images_dir,
+            masks_dir,
+            classes=None,
+            augmentation=None,
+            preprocessing=None,
+    ):
+        self.ids = os.listdir(images_dir)
+        self.images_fps = [os.path.join(images_dir, image_id) for image_id in self.ids]
+        self.masks_fps = [os.path.join(masks_dir, image_id) for image_id in self.ids]
+
+        # convert str names to class values on masks
+        self.class_values = [self.CLASSES.index(cls.lower()) for cls in classes]
+
+        self.augmentation = augmentation
+        self.preprocessing = preprocessing
+
+    def __getitem__(self, i):
+
+        # read data
+        image = cv2.imread(self.images_fps[i])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        mask = cv2.imread(self.masks_fps[i], 0)
+
+        # extract certain classes from mask (e.g. cars)
+        masks = [(mask == v) for v in self.class_values]
+        mask = np.stack(masks, axis=-1).astype('float')
+
+        # apply augmentations
+        if self.augmentation:
+            sample = self.augmentation(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+
+        # apply preprocessing
+        if self.preprocessing:
+            sample = self.preprocessing(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+
+        return image, mask
+
+    def __len__(self):
+        return len(self.ids)
+
+
+# Lets look at data we have
+
+dataset = Dataset(x_train_dir, y_train_dir, classes=['rock'])
+
+image, mask = dataset[5]  # get some sample
+
+'''visualize(
+    image=image,
+    cars_mask=mask.squeeze(),
+)'''
+
+import albumentations as albu
+
+
+def get_training_augmentation():
+    train_transform = [
+
+        albu.HorizontalFlip(p=0.5),
+
+        albu.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=1, border_mode=0),
+
+        albu.PadIfNeeded(min_height=512, min_width=512, always_apply=True, border_mode=0),
+        albu.RandomCrop(height=512, width=512, always_apply=True),
+
+        albu.IAAAdditiveGaussianNoise(p=0.2),
+        albu.IAAPerspective(p=0.5),
+
+        albu.OneOf(
+            [
+                albu.CLAHE(p=1),
+                albu.RandomBrightness(p=1),
+                albu.RandomGamma(p=1),
+            ],
+            p=0.9,
+        ),
+
+        albu.OneOf(
+            [
+                albu.IAASharpen(p=1),
+                albu.Blur(blur_limit=3, p=1),
+                albu.MotionBlur(blur_limit=3, p=1),
+            ],
+            p=0.9,
+        ),
+
+        albu.OneOf(
+            [
+                albu.RandomContrast(p=1),
+                albu.HueSaturationValue(p=1),
+            ],
+            p=0.9,
+        ),
+    ]
+    return albu.Compose(train_transform)
+
+
+def get_validation_augmentation():
+    """Add paddings to make image shape divisible by 32"""
+    test_transform = [
+        albu.PadIfNeeded(512, 512)
+    ]
+    return albu.Compose(test_transform)
+
+
+def to_tensor(x, **kwargs):
+    return x.transpose(2, 0, 1).astype('float32')
+
+
+def get_preprocessing(preprocessing_fn):
+    """Construct preprocessing transform
+
+    Args:
+        preprocessing_fn (callbale): data normalization function
+            (can be specific for each pretrained neural network)
+    Return:
+        transform: albumentations.Compose
+
+    """
+
+    _transform = [
+        albu.Lambda(image=preprocessing_fn),
+        albu.Lambda(image=to_tensor, mask=to_tensor),
+    ]
+    return albu.Compose(_transform)
+
+
+#### Visualize resulted augmented images and masks
+
+augmented_dataset = Dataset(
+    x_train_dir,
+    y_train_dir,
+    augmentation=get_training_augmentation(),
+    classes=['rock'],
+)
+
+# same image with different random transforms
+'''for i in range(5):
+    image, mask = augmented_dataset[1]
+    visualize(image=image, mask=mask.squeeze(-1))'''
+
+import torch
+import numpy as np
+import segmentation_models_pytorch as smp
+
+ENCODER = 'se_resnet101'
+ENCODER_WEIGHTS = 'imagenet'
+CLASSES = ['rock']
+ACTIVATION = 'sigmoid'  # could be None for logits or 'softmax2d' for multiclass segmentation
+DEVICE = 'cuda'
+
+# create segmentation model with pretrained encoder
+model = smp.Unet(
+    encoder_name=ENCODER,
+    encoder_weights=ENCODER_WEIGHTS,
+    classes=len(CLASSES),
+    activation=ACTIVATION,
+)
+
+preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
+
+train_dataset = Dataset(
+    x_train_dir,
+    y_train_dir,
+    augmentation=get_training_augmentation(),
+    preprocessing=get_preprocessing(preprocessing_fn),
+    classes=CLASSES,
+)
+
+valid_dataset = Dataset(
+    x_valid_dir,
+    y_valid_dir,
+    augmentation=get_validation_augmentation(),
+    preprocessing=get_preprocessing(preprocessing_fn),
+    classes=CLASSES,
+)
+
+train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=12, drop_last=True)
+valid_loader = DataLoader(valid_dataset, batch_size=4, shuffle=False, num_workers=4, drop_last=True)
+
+# Dice/F1 score - https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient
+# IoU/Jaccard score - https://en.wikipedia.org/wiki/Jaccard_index
+
+loss = smp.utils.losses.DiceLoss()
+metrics = [
+    smp.utils.metrics.IoU(threshold=0.5),
+]
+
+optimizer = torch.optim.Adam([
+    dict(params=model.parameters(), lr=5e-5),
+])
+
+if hasattr(torch.cuda, 'empty_cache'):
+    torch.cuda.empty_cache()
+
+# create epoch runners
+# it is a simple loop of iterating over dataloader`s samples
+train_epoch = smp.utils.train.TrainEpoch(
+    model,
+    loss=loss,
+    metrics=metrics,
+    optimizer=optimizer,
+    device=DEVICE,
+    verbose=True,
+)
+
+valid_epoch = smp.utils.train.ValidEpoch(
+    model,
+    loss=loss,
+    metrics=metrics,
+    device=DEVICE,
+    verbose=True,
+)
+
+# train model for 40 epochs
+
+max_score = 0
+
+
+for i in range(0, 40):
+
+    print('\nEpoch: {}'.format(i))
+    train_logs = train_epoch.run(train_loader)
+    valid_logs = valid_epoch.run(valid_loader)
+
+    # do something (save model, change lr, etc.)
+    if max_score < valid_logs['iou_score']:
+        max_score = valid_logs['iou_score']
+        torch.save(model, '/home/lvweikun/segmentation_models.pytorch/examples/best_model3.pth')
+        print('Model saved!')
+
+    if i == 25:
+        optimizer.param_groups[0]['lr'] = 1e-6
+        print('Decrease decoder learning rate to 1e-6!')
+
+
+# load best saved checkpoint
+best_model = torch.load('/home/lvweikun/segmentation_models.pytorch/examples/best_model3.pth')
+
+# create test dataset
+test_dataset = Dataset(
+    x_test_dir,
+    y_test_dir,
+    augmentation=get_validation_augmentation(),
+    preprocessing=get_preprocessing(preprocessing_fn),
+    classes=CLASSES,
+)
+
+test_dataloader = DataLoader(test_dataset)
+
+# evaluate model on test set
+test_epoch = smp.utils.train.ValidEpoch(
+    model=best_model,
+    loss=loss,
+    metrics=metrics,
+    device=DEVICE,
+)
+
+logs = test_epoch.run(test_dataloader)
+
+# test dataset without transformations for image visualization
+test_dataset_vis = Dataset(
+    x_test_dir, y_test_dir,
+    classes=CLASSES,
+)
+
+import numpy as np
+from PIL import Image
+import os
+from sklearn.metrics import confusion_matrix
+
+
+def pixelAccuracy(confusion):
+    # return all class overall pixel accuracy
+    # acc = (TP + TN) / (TP + TN + FP + TN)
+    acc = np.diag(confusion).sum() / confusion.sum()
+    return acc
+
+
+def classPixelAccuracy(confusion):
+    # return each category pixel accuracy(A more accurate way to call it precision)
+    # acc = (TP) / TP + FP
+    classAcc = np.diag(confusion) / confusion.sum(axis=1)
+    return classAcc
+
+
+def meanPixelAccuracy(confusion):
+    classAcc = np.diag(confusion) / confusion.sum(axis=1)
+    meanAcc = np.nanmean(classAcc)
+    return meanAcc
+
+
+def meanIntersectionOverUnion(confusion):
+    # Intersection = TP Union = TP + FP + FN
+    # IoU = TP / (TP + FP + FN)
+    intersection = np.diag(confusion)
+    union = np.sum(confusion, axis=1) + np.sum(confusion, axis=0) - np.diag(confusion)
+    IoU = intersection / union
+    mIoU = np.nanmean(IoU)
+    return mIoU
+
+
+def Frequency_Weighted_Intersection_over_Union(confusion):
+    # FWIOU =     [(TP+FN)/(TP+FP+TN+FN)] *[TP / (TP + FP + FN)]
+    freq = np.sum(confusion, axis=1) / np.sum(confusion)
+    iu = np.diag(confusion) / (
+            np.sum(confusion, axis=1) + np.sum(confusion, axis=0) -
+            np.diag(confusion))
+    FWIoU = (freq[freq > 0] * iu[freq > 0]).sum()
+    return FWIoU
+
+
+def Recall(confusion):
+    # return each category pixel accuracy(A more accurate way to call it precision)
+    # acc = (TP) / TP + FP
+    classRecall = np.diag(confusion) / confusion.sum(axis=1)
+    return classRecall
+
+
+all_con_matrix = np.zeros((2, 2))
+for i in range(20):
+    n = np.random.choice(len(test_dataset))
+
+    image_vis = test_dataset_vis[i][0].astype('uint8')
+    image, gt_mask = test_dataset[i]
+
+    gt_mask = gt_mask.squeeze()
+
+    x_tensor = torch.from_numpy(image).to(DEVICE).unsqueeze(0)
+    pr_mask = best_model.predict(x_tensor)
+    pr_mask = (pr_mask.squeeze().cpu().numpy().round())
+
+    visualize(
+        image=image_vis,
+        ground_truth_mask=gt_mask,
+        predicted_mask=pr_mask
+    )
+
+    '''
+    pr_img = Image.fromarray(np.uint8(pr_mask)).resize((512, 512))
+    pr_img = pr_img.convert('RGB')
+    pr_img.save("/home/lvweikun/segmentation_models.pytorch/examples/resnet101_unet/" + str(i) + ".png")
+'''
+    gt_mask = gt_mask.flatten()
+    pr_mask = pr_mask.flatten()
+    one_con_matrix = confusion_matrix(gt_mask, pr_mask)
+    all_con_matrix = all_con_matrix + one_con_matrix
+
+PA = pixelAccuracy(all_con_matrix)
+MPA = meanPixelAccuracy(all_con_matrix)
+MIoU = meanIntersectionOverUnion(all_con_matrix)
+FWIoU = Frequency_Weighted_Intersection_over_Union(all_con_matrix)
+Reccall = Recall(all_con_matrix)
+print("PA:", PA)
+print("MPA:", MPA)
+print("MIoU:", MIoU)
+print("FWIoU:", FWIoU)
+print("Recall:", Reccall)
